91b37e95ae6114c86003f1228a6ff2a0c1107fb1
Thalles Neves <thalles.futurotec.com.br>
2023-12-11 09:15:05 -0300

    Feat : Criação dos dados completos dos poemas Feat : Criação de endpoint para chamar poemas

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const fs = require("fs")
 const cors = require("cors")
 
-const PATH = "./data/db.json"
+const PATH0 = "./data/db.json"
+const PATH1="./data/poems.json"
 const app = express();
 app.use(express.json());
 app.use(cors({origin:true}))
-const rawData = fs.readFileSync(PATH);
+const rawData = fs.readFileSync(PATH0);
+const rawCompleteData = fs.readFileSync(PATH1)
+
 
 const data = JSON.parse(rawData)
+const completeData = JSON.parse(rawCompleteData)
 
 
 
@@ -16,12 +20,7 @@ app.get("/previewpoems",async(req,res)=>{
     return res.status(200).json(data.poemPreview)
 })
 
-app.get("/poem/:poemId",(req,res) => {
-    const {poemId} = req.params;
-    const poemPreview = data.poemPreview
-    const poem = poemPreview.find((poem)=> poem.idLink===poemId);
-    res.status(200).send(poem)
-})
+
 
 app.post("/likepoem/:poemId",(req,res)=>{
     const {poemId} = req.params;
@@ -29,17 +28,23 @@ app.post("/likepoem/:poemId",(req,res)=>{
     var modifiedData = poemPreview;
 
     poemPreview.find((poem,index)=>{  
-        console.log(poem.idLink,"indexed",poemId);
         if(poem.idLink===poemId){
             console.log(modifiedData[index].like);
             modifiedData[index].like = !modifiedData[index].like
         }
     });
     const dadosModificados = JSON.stringify({"poemPreview":modifiedData},null,2)
-    console.log(modifiedData);
-    fs.writeFileSync(PATH,dadosModificados)
+    fs.writeFileSync(PATH0,dadosModificados)
     
     res.status(201).send(dadosModificados)
+})
+
+app.get("/poem/:poemId",(req,res)=>{
+    const {poemId} = req.params;
+
+    const poems = completeData.poems;
+    const poem = poems.find((poem)=>poem.idLink===poemId)
+    res.status(201).send(poem)
 })
 
 app.listen(3001,()=>{
